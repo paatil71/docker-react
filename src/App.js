003096ced6c4042784fddf0d785e5538c2bcf51c
paatil71 <paatil71@gmail.com>
2021-10-08 16:41:11 +0530

    adding on feture

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Priyanka!
+          Hello Priyanka on Travis!
         </p>
         <a
           className="App-link"
